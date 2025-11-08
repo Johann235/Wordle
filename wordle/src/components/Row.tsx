4abc97a7a5b1f8e763601766aa764b1cfcb04608
Johann235 <caancanj@mcmaster.ca>
2025-11-08 17:50:51 -0500

    First commit

---
+++ wordle/src/components/Row.tsx
@@ -0,0 +1,14 @@
+import Square from "./Square.tsx"
+
+export default function Row({Row_Number}){
+    return (
+    <>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>    
+    </>
+    )
+
+}
---
+++ wordle/src/components/Square.tsx
@@ -0,0 +1,8 @@
+export default function Square({Square_number}){
+    return (
+    <>
+    <input type="text" pattern="[A-Z]*" maxLength="1" />
+    </>
+    )
+
+}
